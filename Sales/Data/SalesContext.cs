6196345c0d9efa75059516c3fbd938ca807042c2
Leonardo Bellei <leo_bellei@hotmail.com>
2022-03-01 23:53:59 -0400

    Creating others entities

--- Sales/Data/SalesContext.cs
+++ Sales/Data/SalesContext.cs
@@ -14,6 +14,8 @@ namespace Sales.Data
         {
         }
 
-        public DbSet<Sales.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
