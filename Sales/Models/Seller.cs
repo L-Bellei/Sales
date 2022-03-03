66217504714c883e81cb2d35e05b05572d46fea0
Leonardo Bellei <leo_bellei@hotmail.com>
2022-03-03 07:50:52 -0400

    Inserting validations on Seller edit and create.

--- Sales/Models/Seller.cs
+++ Sales/Models/Seller.cs
@@ -9,24 +9,33 @@ namespace Sales.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "{0} Required!")]
         [Display(Name = "Name")]
         public string Name { get; set; }
+
         [Required]
         [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
         [Required]
-        [Display(Name = "Birthday")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Birth date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate{ get; set; }
+
         [Required]
+        [Range(100.00, 50000.00, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base salary")]
         [DisplayFormat(DataFormatString = "{0:C}")]
         public double BaseSalary { get; set; }
+
         [Display(Name ="Department")]
         public Department Department { get; set; }
+
         [Display(Name ="Department")]
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
