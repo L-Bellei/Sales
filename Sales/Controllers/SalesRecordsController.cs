f77d74d4684847c00150c41a2862a5722aad0c1a
Leonardo Bellei <leo_bellei@hotmail.com>
2022-03-03 08:54:37 -0400

    Implementing a Sales Records search.

---
+++ Sales/Controllers/SalesRecordsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Sales.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
